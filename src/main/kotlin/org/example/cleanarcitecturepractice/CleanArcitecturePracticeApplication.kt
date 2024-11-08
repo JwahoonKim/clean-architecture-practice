5fd5dc314739f09e874636cbced59ae936f341ea
김좌훈 <kjhoon0330@toss.im>
2024-11-08 20:00:51 +0900

    first commit

---
+++ src/main/kotlin/org/example/cleanarcitecturepractice/CleanArcitecturePracticeApplication.kt
@@ -0,0 +1,11 @@
+package org.example.cleanarcitecturepractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CleanArcitecturePracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<CleanArcitecturePracticeApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/cleanarcitecturepractice/CleanArcitecturePracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.cleanarcitecturepractice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CleanArcitecturePracticeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
